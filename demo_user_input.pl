48816c71138430aa3b911200cbf93ed4bc2e7d8e
joe <ngugijoseph992@gmail.com>
2022-06-29 18:50:08 +0200

    updates

--- demo_user_input.pl
+++ demo_user_input.pl
@@ -4,3 +4,5 @@ use warnings;
 
 # Asigning the first arguement passed at the commandline to $dirname
 $dirname = $ARGV[0]
+
+print("We will scan for unmasked credit card numbers in this directory: $dirname\n");
