f74dc6b2964f134d2a8f6b359ee99ebcdc7af57d
joe <ngugijoseph992@gmail.com>
2022-06-29 18:34:15 +0200

    updates

--- hello.pl
+++ hello.pl
@@ -1,2 +1,8 @@
 #!/usr/bin/perl
+# uses of perl
+# Develop CGI scripts
+# Perform test processing tasks
+# Complex batch processing jobs
+# Data Analysis
+# Text manipulation
 print("Hello world\n");
