39c1d4379d4822a6e89109536a4f048bcae82906
joe <ngugijoseph992@gmail.com>
2022-06-29 19:06:00 +0200

    updates

--- variables.pl
+++ variables.pl
@@ -1,8 +1,13 @@
 #!/usr/bin/perl
-
+# Every perl variable must begin with a $ symbol
 use strict;
 use warnings;
+use feature 'say'
 
+# Constants
 use constant num_cards => 4;
-
 print("This is a constant: ",num_cards, "\n");
+
+#
+$card = "Visa"
+say "The card number is: $card";
